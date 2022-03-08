b6196b8b4206f471ec577df03aec8414e2a17088
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-07 21:08:31 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function click() {
+
+  /* Obtiene el sueldo basico */
+  let sueldo = parseInt(document.getElementById("sueldo").value);
+  /* Obtiene la categoria */
+  let categoria = document.getElementById("categoria").value;
+
+  /* TODO */
+  
+}
+
+/* Llama la funcion click al clickear del boton */
+document.getElementById("button").addEventListener("click", click);
