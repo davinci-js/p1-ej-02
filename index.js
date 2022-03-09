92e5b4c0f5941546ca5c70e606abd83169827558
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-08 22:47:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 "use strict";
 
-function clicked() {
-
-  /* Obtiene la categoria del sueldo */
-  let categoria = parseInt(document.getElementById("categoria").value);
-  /* Obtiene la categoria */
-  let subcategoria = document.getElementById("subcategoria").value;
-
-  /* TODO */
-  
-}
+/* TODO */
